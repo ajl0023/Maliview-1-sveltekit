96f0cc12a271b8c282dc82629b3d379f2c522f41
ajl0023 <austinlee199723@yahoo.com>
2021-09-15 23:54:27 -0700

    removed folders

--- commands.bat
+++ commands.bat
@@ -1,2 +1,1 @@
- cd client && npm run build && cd ..&& git add . && git commit -m "uploading temporary bg photo" && git push origin main
- cd client && npm run build && cd ..&& git add . && git commit -m "svg not showing up in safari fixed" && git push origin main
+ cd client && npm run build && cd ..&& git add . && git commit -m "untracked files and folders" && git push origin main
