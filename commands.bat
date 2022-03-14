f1da439c41745978a4f94deabfe9bcd502062d73
ajl0023 <austinlee199723@yahoo.com>
2022-03-14 15:55:55 -0700

    changes in file structure

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
- cd client && npm run build && cd ..&& git add . && git commit -m "images are now from server" && git push origin main
+cd client && git add . && git commit -m "images are now from server" && git push origin main
